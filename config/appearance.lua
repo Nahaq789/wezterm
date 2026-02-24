b6ff1c7d0f5a05a54f881336d5bcb345d4105f53
Nahaq789 <naha022677@icloud.com>
2026-02-24 15:37:53 +0900

    fix: cursor thickness

--- config/appearance.lua
+++ config/appearance.lua
@@ -9,7 +9,7 @@ return {
    webgpu_preferred_adapter = gpu_adapters:pick_best(),
    -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Dx12', 'IntegratedGpu'),
    -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Gl', 'Other'),
-   underline_thickness = '1.5pt',
+   underline_thickness = '1px',
 
    -- cursor
    animation_fps = 120,
