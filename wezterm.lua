ea90426995b2d416aa967583e159e44fd969396e
Nahaq789 <naha022677@icloud.com>
2026-08-03 11:34:59 +0900

    fix

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,7 @@
+local wezterm = require('wezterm')
 local Config = require('config')
 
 require('utils.backdrops')
-   -- :set_focus('#000000')
-   -- :set_images_dir(require('wezterm').home_dir .. '/Pictures/Wallpapers/')
    :set_images()
    :random()
 
@@ -11,10 +10,25 @@ require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
 require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'circle' })
 require('events.new-tab-button').setup()
 
+wezterm.on('gui-startup', function(cmd)
+   local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+   -- 右側に分割
+   pane:split({
+      direction = 'Right',
+      size = 0.5,
+   })
+end)
+
 return Config:init()
    :append(require('config.appearance'))
    :append(require('config.bindings'))
    :append(require('config.domains'))
    :append(require('config.fonts'))
    :append(require('config.general'))
-   :append(require('config.launch')).options
+   :append(require('config.launch'))
+   :append({
+      set_environment_variables = {
+         no_proxy = 'localhost,127.0.0.1,::1',
+         NO_PROXY  = 'localhost,127.0.0.1,::1',
+      },
+   }).options
